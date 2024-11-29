5b2819cf562d50cc81d2ed7442e5edfb8a8bb883
bishal-dd <dhakalbishal930@gmail.com>
2024-11-29 23:33:24 +0600

    remove jwt protection

--- middleware.go
+++ middleware.go
@@ -2,34 +2,35 @@ package main
 
 import (
 	"github.com/bishal-dd/receipt-generator-backend/helper/contextUtil"
-	"github.com/bishal-dd/receipt-generator-backend/helper/jwtUtil"
 	"github.com/gin-gonic/gin"
 )
 
 
 func GinContextToContextMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		tokenString, err := jwtUtil.TokenFromRequest(c.Request)
-		if err != nil {
-			c.AbortWithStatusJSON(401, gin.H{"error": err.Error()})
-			return
-		}
-		token, err := jwtUtil.ParseToken(tokenString)
-		if err != nil {
-			c.AbortWithStatusJSON(401, gin.H{"error": err.Error()})
-			return
-		}
-		claims, err := jwtUtil.ClaimsFromToken(token)
-		if err != nil {
-			c.AbortWithStatusJSON(401, gin.H{"error": "invalid token"})
-			return
-		}
+		// tokenString, err := jwtUtil.TokenFromRequest(c.Request)
+		// if err != nil {
+		// 	c.AbortWithStatusJSON(401, gin.H{"error": err.Error()})
+		// 	return
+		// }
+		// token, err := jwtUtil.ParseToken(tokenString)
+		// if err != nil {
+		// 	c.AbortWithStatusJSON(401, gin.H{"error": err.Error()})
+		// 	return
+		// }
+		// claims, err := jwtUtil.ClaimsFromToken(token)
+		// if err != nil {
+		// 	c.AbortWithStatusJSON(401, gin.H{"error": "invalid token"})
+		// 	return
+		// }
 
-		userID, ok := claims["user_id"].(string)
-		if !ok {
-			c.AbortWithStatusJSON(401, gin.H{"error": "user_id not found in token"})
-			return
-		}
+		// userID, ok := claims["user_id"].(string)
+		// if !ok {
+		// 	c.AbortWithStatusJSON(401, gin.H{"error": "user_id not found in token"})
+		// 	return
+		// }
+
+		userID := "user_2aJJmO34aZqwe1eqxFwsFs0WAs1wqw"
 		ctx := contextUtil.SetContextValue(c.Request.Context(), contextUtil.UserIDKey, userID)
 		ctx = contextUtil.SetContextValue(ctx, contextUtil.GinContextKey, c)
 		c.Request = c.Request.WithContext(ctx)
